8529936d3d9137f8f8b0ce98678a1eba5a75ae3a
kkarakamis <kutaykarakamis@hotmail.com>
2021-06-07 18:32:31 +0300

    Buttons initialised and layout arranged

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        recordLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+    }
+    
 }
 
