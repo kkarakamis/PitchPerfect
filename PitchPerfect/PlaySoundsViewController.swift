71d969469090f7297b98f55378def82b94c45d63
kkarakamis <kutaykarakamis@hotmail.com>
2021-07-25 14:07:41 +0300

    UI and audio code wired. App is working correctly

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -25,19 +25,34 @@ class PlaySoundsViewController: UIViewController {
     var stopTimer: Timer!
 
     enum ButtonType: Int {
-        case slow = 0, fast, chipmunk, vader, echo, reverb
+        case slow = 0, fast, vader, chipmunk, echo, reverb
     }
 
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        print("Play Audio Button Pressed")
-    }
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+
+        configureUI(.playing)    }
 
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
-        print("Stop Audio Button Pressed")
+        stopAudio()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAudio()
         
 //        shiftButton.contentMode = .Center
 //        shiftButton.imageView?.contentMode = .ScaleAspectFit
@@ -45,8 +60,8 @@ class PlaySoundsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(<#T##animated: Bool##Bool#>)
-
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
 
 }
